a3ec50523770a0a3cd608e7563d6e8e984f20b65
Varjus <varjus35@gmail.com>
2022-09-29 17:00:18 -0300

    Primeiro commit, criacao de login e tela inicial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { AuthenticationService } from '../services/authentication.service';
+import { AvatarService } from '../services/avatar.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  profile = null;
+  
+  constructor(
 
-  constructor() {}
+    private avatarService: AvatarService,
+    private authenticationService: AuthenticationService,
+    private router: Router,
+    private loadingController: LoadingController,
+    private alertController: AlertController,
+
+  ) {}
+async logout() {
+  this.authenticationService.logout();
+  this.router.navigateByUrl('/', { replaceUrl: true });
+} 
+
+async changeImage(){
+
+}
 
 }
