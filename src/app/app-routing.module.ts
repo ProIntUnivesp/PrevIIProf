274923637f8b24b9f31baf38e2a7e69ec3b110da
Varjus <varjus35@gmail.com>
2022-10-20 11:17:25 -0300

    Acerto de rotas quando não estiver logado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AuthGuard } from './guards/auth.guard';
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import {
@@ -12,7 +13,7 @@ import {
     RouterModule.forRoot([
       { path: '', redirectTo: 'login', pathMatch: 'full' },
       { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule) },
-      { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
+      { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule), canActivate:[AuthGuard] },
       { path: 'calcprevprofsp', loadChildren: () => import('./calcprevprofsp/calcprevprofsp.module').then(m => m.CalcprevprofspPageModule) }
     ])
   ],
